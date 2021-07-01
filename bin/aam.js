daf56068d5689ed58783c10b4a3a1c544b29ab5e
Melvin Carvalho <melvincarvalho@gmail.com>
2021-07-01 15:46:13 +0200

    add aam create

--- bin/aam.js
+++ bin/aam.js
@@ -8,7 +8,8 @@ const { aam_search } = require('../lib/')
 // MODEL
 globalThis.data = {
   command: 'search',
-  term: 'aam'
+  term: 'aam',
+  defaultCreate: 'template'
 }
 // console.log('data', data, argv)
 
@@ -22,6 +23,9 @@ if (data.command === 'search') {
   console.log(res)
 } else if (data.command === 'install') {
   let res = aam_search(data.term)
+  console.log('git clone ' + res?.repository)
+} else if (data.command === 'create') {
+  let res = aam_search(data.defaultCreate)
   console.log('git clone ' + res?.repository)
 } else {
   console.log('command not found')
