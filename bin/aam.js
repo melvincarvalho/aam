94a91a5967f617935d3598af5d4473313269a4d7
Melvin Carvalho <melvincarvalho@gmail.com>
2021-07-03 16:24:06 +0200

    add messages to install and create

--- bin/aam.js
+++ bin/aam.js
@@ -9,24 +9,36 @@ const { aam_search } = require('../lib/')
 globalThis.data = {
   command: 'search',
   term: 'aam',
-  defaultCreate: 'template'
+  defaultCreate: 'template',
+  directory: 'template'
 }
 // console.log('data', data, argv)
 
 // INIT
 data.command = argv._[0] || data.command
-data.term = argv._[1] || data.term
 
 // MAIN
 if (data.command === 'search') {
   let res = aam_search(data.term)
+  data.term = argv._[1] || data.term
   console.log(res)
 } else if (data.command === 'install') {
-  let res = aam_search(data.term)
-  console.log('git clone ' + res?.repository)
+  data.term = argv._[1] || data.term
+  data.directory = argv._[2] || data.directory
+  const res = aam_search(data.term)
+  console.log(res)
+  console.log('run:')
+  console.log('git clone ' + res?.repository + ' ' + (argv._[2] || ''))
+  console.log('cd ' + (argv._[2] || data.term))
+  console.log('npm install')
+  console.log('npm run')
 } else if (data.command === 'create') {
   let res = aam_search(data.defaultCreate)
-  console.log('git clone --depth 1 ' + res?.repository)
+  data.directory = argv._[1] || data.directory
+  console.log('run:')
+  console.log('git clone --depth 1 ' + res?.repository + ' ' + (argv._[1] || ''))
+  console.log('cd ' + data.directory)
+  console.log('rm -rf .git')
 } else {
   console.log('command not found')
 }
